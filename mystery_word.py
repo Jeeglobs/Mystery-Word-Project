079b9928dd0ddcf87a70dd6fba7cfc06a3c846ad
Jeeglobs <dp.hudgins@hotmail.com>
2023-02-16 15:42:10 -0500

    following along with Rebecca in class

--- mystery_word.py
+++ mystery_word.py
@@ -1,6 +1,18 @@
 def play_game():
-    pass
+    word = 'apple'
+    wrong_guesses = ['b', 'd']
+    right_guesses = ['a', 'p']
 
+    game_board = ''
+    for letter in word:
+        if letter in right_guesses:
+            game_board += letter + ' '
+        else:
+            game_board += '_ '
+    print(game_board)
+
+
+# DO NOT CHANGE CODE BELOW THIS LINE!!!
 
 if __name__ == "__main__":
     play_game()
