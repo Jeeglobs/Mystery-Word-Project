cc93d24069c5ac77301506bad84594614f1ff1e0
Jeeglobs <dp.hudgins@hotmail.com>
2023-02-19 15:40:47 -0500

    cleaned code up a bit; started playing around with difficulty options

--- mystery_word.py
+++ mystery_word.py
@@ -1,8 +1,27 @@
 import random
 
-# should be able to delete lower case now that player_guess is capitalized when submitted
+# list of capitalized letters; will be used as possible_letters
 ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
             'Z']
+
+# # SEPARATE WORDS INTO LISTS BASED ON LENGTH
+# def create_words_lists(file):
+#     easy_words = []
+#     medium_words = []
+#     hard_words = []
+#     insane_words = []
+#     with open(file) as opened_file:
+#         words_list = opened_file.read().splitlines()
+#         for word in words_list:
+#             if len(word) > 2 and len(word) < 5:
+#                 easy_words.append(word)
+#             elif len(word) > 4 and len(word) < 7:
+#                 medium_words.append(word)
+#             elif len(word) > 6 and len(word) < 9:
+#                 hard_words.append(word)
+#             elif len(word) > 8:
+#                 insane_words.append(word)
+#     return easy_words, medium_words, hard_words, insane_words
 
 
 def select_word(file):
@@ -13,6 +32,7 @@ def select_word(file):
     return game_word
 
 
+# MAIN FUNCTION
 def play_game():
 
     game_word = select_word('words.txt')
@@ -23,6 +43,7 @@ def play_game():
     guessed_letters = []
     possible_letters = ALPHABET
 
+    # tried to figure out how to have this function outside of play_game and call it
     def update_word_display():
         blank_word = ''
         for letter in game_word:
@@ -33,16 +54,19 @@ def play_game():
         print(blank_word)
         return blank_word
 
+    # made this to simplify code
+    def update_game_board():
+        print(' \n ')
+        print("Letters Guessed: " + str(guessed_letters))
+        print("Chances Left: " + str(remaining_guesses))
+
     # OPENING GAME BOARD
-    print(' \n ')
-    print("Welcome to Mystery Word!")
-    print("Letters Guessed: " + str(guessed_letters))
-    print("Chances Left: " + str(remaining_guesses))
+    update_game_board()
     update_word_display()
+    print("Welcome to Mystery Word!")
     print("Ready to play?")
 
     while remaining_guesses > 0:
-        # for letter in game_word:
         player_guess = input("Guess a letter! ").upper()
         if player_guess not in possible_letters:
             print("Your guess must be a single letter. Try again!")
@@ -54,9 +78,7 @@ def play_game():
             fixed_game_letters.remove(player_guess)
             guessed_letters.append(player_guess)
             chosen_game_letters.append(player_guess)
-            print(' \n ')
-            print("Letters Guessed: " + str(guessed_letters))
-            print("Chances Left: " + str(remaining_guesses))
+            update_game_board()
             update_word_display()
 
             if len(fixed_game_letters) == 0:
@@ -68,16 +90,12 @@ def play_game():
         else:
             guessed_letters.append(player_guess)
             remaining_guesses -= 1
-            print(' \n ')
-            print("Letters Guessed: " + str(guessed_letters))
-            print("Chances Left: " + str(remaining_guesses))
+            update_game_board()
             update_word_display()
             print("Oops! Try again!")
 
     else:
-        print(' \n ')
-        print("Letters Guessed: " + str(guessed_letters))
-        print("Chances Left: " + str(remaining_guesses))
+        update_game_board()
         print("The mystery word was: " + game_word)
         print("YOU LOSE!")
 
